e25a805e2786c46a283e0e7c095d11d74887fa01
Wataru Fuchu <fuchuwat@gmail.com>
2018-07-01 03:03:01 +0900

    20180630 commit

---
+++ ch4/str1.py
@@ -0,0 +1,13 @@
+# test1
+print('======test======')
+print('''Hello !!!!
+Multi Line Test!!!!''')
+
+# test2
+print('======test======')
+print('''Hello !!!!
+            Multi Line Test!!!!''')
+
+# test3
+print('======test======')
+print('''Hello !!!!\nMulti Line Test!!!!''')
---
+++ ch4/str2.py
@@ -0,0 +1,14 @@
+a = 'He'
+b = 'llo'
+
+print('print(a + b)')
+print(a + b)
+
+print('print(a, b)')
+print(a, b)
+
+# 繰り返し
+start  = 'わく' * 4 + '\n'
+middle = 'ドキ' * 3 + '\n'
+end    = '楽しいなー'
+print(start + middle + end)
